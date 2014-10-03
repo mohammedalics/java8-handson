390534a6243233ce800066fa9c20982719a304de
Mohammed Ali <mohammed.ali.cs@gmail.com>
2014-10-04 01:28:45 +0200

    Ambiguity example

---
+++ src/main/java/home/handson/java8/Ambiguity.java
@@ -0,0 +1,38 @@
+package home.handson.java8;
+
+// TODO: Auto-generated Javadoc
+interface Amb1 {
+	public String method();
+}
+
+interface Amb2 {
+	public String method();
+}
+
+class MyClass {
+	public void method(Amb1 amb1) {
+
+	}
+
+	public void method(Amb2 amb2) {
+
+	}
+
+}
+
+/**
+ * The Class Ambiguity.
+ */
+public class Ambiguity {
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		MyClass myClass = new MyClass();
+		myClass.method((Amb1) () -> "Hello");
+	}
+
+}
