6cbca6a9c269c9ee4dc040661ac99e5c7a0c65a9
Mohammed Ali <mohammed.ali.cs@gmail.com>
2014-10-03 22:16:50 +0200

    Add LambdaWithVariables

---
+++ src/main/java/home/handson/java8/LambdaWithVariables.java
@@ -0,0 +1,17 @@
+package home.handson.java8;
+
+public class LambdaWithVariables {
+	
+	public static void main (String[] args) {
+		int x = 2; // the same as final.  
+		HelloService helloService = (firstName, lastName) -> {
+			// can't re-assign x
+			// curly brackets of lambda is not scope. the outer scope is the scope of lambda.  
+			// x = 3; 
+			return "Hello " + firstName + " " + lastName + "!";
+		};
+				
+		System.out.println(helloService.sayHello(args[0], args[1]));
+	}
+
+}
