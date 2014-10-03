04fb7ea617e36241ded5a4d19b408cfce7fb9ded
Mohammed Ali <mohammed.ali.cs@gmail.com>
2014-10-03 20:26:07 +0200

    Add HelloCallable, simple demonstration of lambda using Callable interface (built-in interface).

---
+++ src/main/java/home/handson/java8/HelloCallable.java
@@ -0,0 +1,31 @@
+package home.handson.java8;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Example of functional interfaces with Lambda
+ * 
+ *
+ */
+public class HelloCallable {
+
+   public static void main(String[] args) {
+      try {
+    	  // Java8
+         Callable<String> c = () -> "Hello from Callable";
+
+           // Java7
+//         Callable<String> c = new Callable<String>() {
+//			
+//			@Override
+//			public String call() throws Exception {
+//				
+//				return "Hello from Callable";
+//			}
+//		};
+         System.out.println(c.call());
+      } catch (Exception e) {
+         System.err.println(e.getMessage());
+      }
+   }
+}
