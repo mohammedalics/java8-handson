74deb5070dbeb304446404c7a23adadce6a0dea6
Mohammed Ali <mohammed.ali.cs@gmail.com>
2014-10-03 22:33:44 +0200

    Functional Interface With Array As Parameter.

---
+++ src/main/java/home/handson/java8/FunctionalInterfaceWithArrayAsParameter.java
@@ -0,0 +1,25 @@
+package home.handson.java8;
+
+/**
+ * 
+ * @author mohammedali
+ *
+ */
+
+interface Int {
+	void setInt(int[] i); 
+	
+}
+public class FunctionalInterfaceWithArrayAsParameter {
+
+	public static void main(String[] args) {
+		Int int1  =(int... i)->{
+		
+			for (int j = 0; j < i.length; j++) {
+				System.out.println(i[j]);
+			}
+		};
+		int1.setInt(new int[] {1, 2, 3});
+	}
+	
+}
