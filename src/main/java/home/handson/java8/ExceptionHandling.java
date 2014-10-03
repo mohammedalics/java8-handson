51f4f7d89dc70c01c151898626fdf794aac1de7d
Mohammed Ali <mohammed.ali.cs@gmail.com>
2014-10-03 23:11:04 +0200

    Add Exception handling Example

---
+++ src/main/java/home/handson/java8/ExceptionHandling.java
@@ -0,0 +1,27 @@
+package home.handson.java8;
+
+
+interface IService {
+	void method() throws Exception;
+}
+
+public class ExceptionHandling {
+	// If the lambda throw an exception, the functional interface should throws the same exceptions. 
+	public static void main(String[] args) {
+		IService service = () -> {
+			throw new Exception(); 
+			
+		};
+		
+		// Compilation error should occur if you call a method from 'service' without handling the exception. 
+
+		try {
+			service.method();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+			
+	}
+	
+
+}
