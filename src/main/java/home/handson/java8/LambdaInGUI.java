ba4c65deab55099298a040adc4940d6c1f4772b9
Mohammed Ali <mohammed.ali.cs@gmail.com>
2014-10-03 23:30:39 +0200

    Lambda In GUI

---
+++ src/main/java/home/handson/java8/LambdaInGUI.java
@@ -0,0 +1,38 @@
+package home.handson.java8;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class LambdaInGUI {
+
+	public static void main(String[] args) {
+		new MyFrame().setVisible(true);
+	}
+}
+
+
+class MyFrame extends JFrame {
+	JButton btn; 
+	public MyFrame() {
+		setSize(150, 150);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		btn = new JButton("Show Hello");
+		add(btn);
+		// Java7 
+//		btn.addActionListener(new ActionListener() {
+//			
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				JOptionPane.showMessageDialog(null, "Hello!!");
+//				
+//			}
+//		});
+		
+		// java 8
+		btn.addActionListener((e) -> JOptionPane.showMessageDialog(null, "Hello!!"));
+	}
+}
