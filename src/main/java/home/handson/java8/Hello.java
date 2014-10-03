0ed46b96db035f08c4a97b8da06fa5cefb1c6543
Mohammed Ali <mohammed.ali.cs@gmail.com>
2014-10-03 17:35:38 +0200

    add comments.

--- src/main/java/home/handson/java8/Hello.java
+++ src/main/java/home/handson/java8/Hello.java
@@ -1,6 +1,5 @@
 package home.handson.java8;
 
-
 /**
  * Hello Lambda!
  *
@@ -13,40 +12,43 @@ package home.handson.java8;
  */
 
 interface HelloService {
-		String sayHello(String firstName, String lastName);
+	String sayHello(String firstName, String lastName);
 }
 
 /**
  * The Class HelloServiceImpl.
  */
-//class HelloServiceImpl implements HelloService {
-//	@Override
-//	public String sayHello(String firstName, String lastName) {
-//		return "Hello " + firstName + " " + lastName + "!";
+// class HelloServiceImpl implements HelloService {
+// @Override
+// public String sayHello(String firstName, String lastName) {
+// return "Hello " + firstName + " " + lastName + "!";
 //
-//	}
-//}
+// }
+// }
 
 /**
  * The Class Hello.
  */
 public class Hello {
-	
+
 	/**
 	 * The main method.
 	 *
-	 * @param args the arguments
+	 * @param args
+	 *            the arguments
 	 */
 	public static void main(String[] args) {
 
 		// java8
-		// Parameter type String for firstName and lastName is optional. 
+		// Parameter type String for firstName and lastName is optional.
+		// Curly braces are optional because it's only one statement.
 		HelloService helloService = (firstName, lastName) -> {
 			return "Hello " + firstName + " " + lastName + "!";
 		};
+				
 
 		// Java7
-//		HelloService helloService = new HelloServiceImpl();
+		// HelloService helloService = new HelloServiceImpl();
 		System.out.println(helloService.sayHello(args[0], args[1]));
 
 	}
