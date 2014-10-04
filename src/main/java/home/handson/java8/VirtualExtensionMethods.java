2e3c2ad5e4557d68176eb98f948a0d77f077ff6e
Mohammed Ali <mohammed.ali.cs@gmail.com>
2014-10-04 02:20:05 +0200

    default implementation for interface methods

---
+++ src/main/java/home/handson/java8/VirtualExtensionMethods.java
@@ -0,0 +1,24 @@
+package home.handson.java8;
+
+interface InterfaceA {
+	public void method1 (); 
+	default public void method2() {
+		System.out.println("Hello Default Implementation");
+	}
+}
+public class VirtualExtensionMethods {
+	
+	public static void main (String[] args) {
+		InterfaceA a1 = new InterfaceA() {
+			
+			@Override
+			public void method1() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		
+		a1.method2();
+	}
+
+}
