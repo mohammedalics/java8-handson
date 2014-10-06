d606412d65d0f8bcd2a7fc7ad0c9b09e6ba5a326
Mohammed Ali <mohammed.ali.cs@gmail.com>
2014-10-06 21:28:50 +0200

    Add MultipleInterfaces

---
+++ src/main/java/home/handson/java8/defaultmethods/MultipleInterfaces.java
@@ -0,0 +1,23 @@
+package home.handson.java8.defaultmethods;
+
+ interface A1 {
+    default void foo(){
+       System.out.println("Calling A.foo()");
+    }
+}
+ 
+ interface B1 {
+    default void foo1(){
+       System.out.println("Calling B.foo()");
+    }
+}
+
+ 
+ /**
+  * Clazz1 can't implement two interfaces have two default methods with the same signature. 
+  * complication error occur 
+  * @author mohammedali
+  *
+  */
+class Clazz1 implements A1, B1 {
+}
